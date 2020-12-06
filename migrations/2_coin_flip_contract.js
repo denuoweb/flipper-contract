d42f648182f2f5d93d94f11daa3135621a48f64e
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-12-06 14:38:38 +0200

    tryout ropsten deploy

--- migrations/2_coin_flip_contract.js
+++ migrations/2_coin_flip_contract.js
@@ -1,5 +1,5 @@
 const CoinFlipContract = artifacts.require("CoinFlipContract");
 
 module.exports = function (deployer) {
-  deployer.deploy(CoinFlipContract, { value: web3.utils.toWei('10', 'ether') });
+  deployer.deploy(CoinFlipContract, { value: web3.utils.toWei('1', 'ether') });
 };
